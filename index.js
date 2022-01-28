f5cbe8c857b38194adf3689c3ed4501bde8c42d0
DatNC <datnc@younetgroup.com>
2022-01-28 11:02:13 +0700

    update for shotscreen

--- index.js
+++ index.js
@@ -1,18 +1,2 @@
-// file 24h.js
-const puppeteer = require('puppeteer');
-
-(async () => {
-// mở trình duyệt
-  const browser = await puppeteer.launch({ headless: false });
-  // Mở 1 page mới
-  const page = await browser.newPage();
-  // đi đến trang 24h
-  await page.goto('https://24h.com.vn');
-  // chụp ảnh màn hình và lưu lại với tên 24h.png
-  await page.screenshot({path: './img/24h.png'});
-  // Lưu ảnh màn hình thành file pdf
-//   await page.pdf({path: 'hn.pdf', format: 'A4'});
-
-// tắt trình duyệt
-  await browser.close();
-})();
+const shotScreen = require("./shotScreen");
+shotScreen();
